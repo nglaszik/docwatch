8763b5db2835ccbec3a7d07b85e66d7ed93dc9e5
Nandor Laszik <nglaszik@gmail.com>
2025-04-18 08:16:57 -0700

    Add migrations

---
+++ migrations/20250413232041_init.sql
@@ -0,0 +1,38 @@
+-- Create users table.
+create table if not exists users
+(
+	id integer primary key not null,
+	username text not null unique,
+	password text not null
+);
+
+-- Create sessions table.
+CREATE TABLE sessions (
+	user_id INTEGER NOT NULL,
+	token TEXT PRIMARY KEY,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- monitored documents table
+CREATE TABLE IF NOT EXISTS documents (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	user_id INTEGER NOT NULL,
+	doc_id TEXT NOT NULL UNIQUE,
+	last_updated TEXT NOT NULL,
+	name TEXT NOT NULL DEFAULT 'Untitled',
+	latest_content TEXT,
+	export_link TEXT,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE document_revisions (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  document_id INTEGER NOT NULL,
+  revision_time TEXT NOT NULL,
+  content TEXT NOT NULL,
+  diff TEXT,
+  added_chars INTEGER DEFAULT 0,
+  deleted_chars INTEGER DEFAULT 0,
+  FOREIGN KEY(document_id) REFERENCES documents(id)
+);
